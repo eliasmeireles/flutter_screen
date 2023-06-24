67f288cba42642fc7147104def0aacee03924bcb
Elias Meireles <eliasmflilico@gmail.com>
2023-06-24 18:02:27 -0300

    Import svg fix

--- lib/src/util/assets_util.dart
+++ lib/src/util/assets_util.dart
@@ -4,11 +4,11 @@ class AssetsUtil {
   }
 
   static String loadScreenViewSvgBackground(String svgName) {
-    return 'submodules/flutter_screen/assets/svg/background/$svgName.svg';
+    return 'assets/svg/background/$svgName.svg';
   }
 
   static String loadScreenViewSvgAlert(String svgName) {
-    return 'submodules/flutter_screen/assets/svg/alert/$svgName.svg';
+    return 'assets/svg/alert/$svgName.svg';
   }
 
   static String loadSvgAlert(String svgName) {
