fb951735215e0f288317d59146b2f2cd47e6ea33
Elias Meireles <eliasmflilico@gmail.com>
2023-06-24 18:45:13 -0300

    Fixed base svg ref

--- lib/src/util/assets_util.dart
+++ lib/src/util/assets_util.dart
@@ -1,17 +1,20 @@
 class AssetsUtil {
   static String loadSvgBackground(String svgName) {
-    return 'assets/svg/background/$svgName.svg';
+    return 'assets/packages/flutter_screen/lib/svg/background/$svgName.svg';
   }
 
   static String loadScreenViewSvgBackground(String svgName) {
-    return 'assets/svg/background/$svgName.svg';
+    return 'assets/packages/flutter_screen/lib/svg/background/$svgName.svg';
   }
 
   static String loadScreenViewSvgAlert(String svgName) {
-    return 'assets/svg/alert/$svgName.svg';
+    return 'assets/packages/flutter_screen/lib/svg/alert/$svgName.svg';
   }
 
   static String loadSvgAlert(String svgName) {
-    return 'assets/svg/alert/$svgName.svg';
+    return 'assets/packages/flutter_screen/lib/svg/alert/$svgName.svg';
+  }
+  static String loadAsset(String svgName) {
+    return '$svgName';
   }
 }
